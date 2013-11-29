4bf3d4ef71e668d54fbc6bed4baedc65fec77ff0
Daniel Janczak <daniel_janczak@yahoo.com>
2013-11-29 11:16:55 +1000

    Adding Program controller and Index view and setting it as default start view

--- TestMvc/App_Start/RouteConfig.cs
+++ TestMvc/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TestMvc
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Program", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ TestMvc/Controllers/ProgramController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace TestMvc.Controllers
+{
+    public class ProgramController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+
+            return View();
+        }
+    }
+}
